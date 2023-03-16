46553452b3be81b18f5edcfed950bf063143df22
Nurken Zhappargaly <zhnurken@gmail.com>
2023-03-16 18:06:02 +0600

    add modal

--- functions.php
+++ functions.php
@@ -10,7 +10,22 @@ function childhood_styles() {
 
 function childhood_scripts() {
     wp_enqueue_script( 'childhood-scripts', get_template_directory_uri() . '/assets/js/main.min.js', array('jquery'), null, true );
+
+    wp_deregister_script( 'jquery' );
+    wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js' );
+
+    wp_enqueue_script('jquery');
 };
 
 add_theme_support('custom-logo');
 add_theme_support('post-thumbnails');
+
+function my_acf_google_map_api( $api ){
+	
+	$api['key'] = 'AIzaSyA0wh-iveKMSWHxlV-yyRrPIVHH7tzEHzQ'; // Ваш ключ Google API
+	
+	return $api;
+	
+}
+
+add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
